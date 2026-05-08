62b9b33925eec25f307251f63b3e5b472d17e4ad
MelchorRuiz <ruizgonzalezmelchor3@gmail.com>
2026-05-07 22:23:40 -0600

    07/05/2026 - Tipos de datos

---
+++ ejemplo1/temaVariables.kts
@@ -0,0 +1,15 @@
+fun main(){
+    val saludo: String = "Hola"
+    var nombre: String = "Kotlin"
+    println("$saludo $nombre")
+
+    println("Ingresa el primer número")
+    val num1 = readln().toInt()
+    println("Ingresa el segundo número")
+    val num2 = readln().toInt()
+
+    var res = num1 + num2
+    println("La suma es: $res")
+}
+
+main()
